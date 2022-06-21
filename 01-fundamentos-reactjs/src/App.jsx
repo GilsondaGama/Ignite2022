f76c2955ea35d73fa8c9a9f3a867b4997cf78686
Gilson da Gama <gilsondagama@hotmail.com>
2022-06-21 18:04:23 -0300

    Iniciando primeiro projeto em ReactJS

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,14 @@
+import { styles } from "./components/Header"
+
+import './global.css'
+
+function App() {
+
+  return (
+    <Header>
+      <h1>Hello world</h1>
+    </Header>
+  )
+}
+
+export default App
