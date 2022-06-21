212f91b99ce15f1c5f509848cf0a09a8d3f38e13
Gilson da Gama <gilsondagama@hotmail.com>
2022-06-21 18:42:10 -0300

    Component Header, CSS iniciais

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,5 +1,13 @@
+import styles from './Header.module.css'
+
+import ignitelogo from '../assets/ignite-logo.svg'
+
 export function Header() {
   return (
-    <strong>Ignite Feed</strong>
+    <header className={styles.header}>
+      <img src={ignitelogo} alt="Logo do Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
+
   )
 }
