c4fa3a937df530a89a7b7dc31e80211a44954a8b
Gilson da Gama <gilsondagama@hotmail.com>
2022-06-23 16:34:00 -0300

    Iteração no JSX e propriedades do post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,8 +3,38 @@ import { Post } from "./components/Post"
 import { Sidebar } from "./components/Sidebar"
 
 import styles from './App.module.css'
-
 import './global.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/32863365?v=4',
+      name: 'Gilson da Gama',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Learn everything there '},
+      {type: 'paragraph', content: 'from its medieval origins to the digital era'},
+      {type: 'link', content: 'is to know about the ubiquitous lorem ipsum passage', href: 'https://www.google.com'},
+    ],
+    publishedAt: new Date('2022-6-23 08:13:35'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://xesque.rocketseat.dev/users/avatar/profile-cf6ee6bd-69bc-4472-9dd0-c4403f229adc.jpg',
+      name: 'Diego Fernandes',
+      role: 'CTO Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Learn everything there '},
+      {type: 'paragraph', content: 'from its medieval origins to the digital era'},
+      {type: 'link', content: 'is to know about the ubiquitous lorem ipsum passage', href: 'https://www.google.com'},
+    ],
+    publishedAt: new Date('2022-06-21 18:10:15'),
+  },
+]
 
 function App() {
   return (
@@ -12,11 +42,16 @@ function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author} 
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
 
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,29 +1,58 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR' 
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({author, content, publishedAt}) {
+  const publishedDateFormatted = 
+    format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+      locale: ptBR
+    }
+  )
+
+  const publishedDateRelativetoNow = 
+    formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+      }
+    )
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/32863365?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Gilson da Gama</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="15 de Julho às 8:13h" dateTime="2022-05-11 08:13:35">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime = {publishedAt.toISOString()}>
+          {publishedDateRelativetoNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Learn everything there </p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
+
+
+
+        {/* <p>Learn everything there </p>
         <p>from its medieval origins to the digital era</p>
         <p>is to know about the ubiquitous lorem ipsum passage</p>
         <a href=''>
           <p>anim id est laborum</p>
-        </a>
+        </a> */}
       </div>
 
       <form className={styles.commentForm}>
