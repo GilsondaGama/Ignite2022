1e62b10cb1d49decfeb3fcf9f6b41229d62af1f2
Gilson da Gama <gilsondagama@hotmail.com>
2022-06-22 21:12:07 -0300

    Criando o component Comment

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/32863365?v=4" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gilson da Gama</strong>
+              <time title="15 de Julho às 8:13h" dateTime="2022-05-11 08:13:35">Cerca de uma hora atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20}/>
+            </button>
+          </header>
+          
+          <p>Muito bom devgon, parabén!!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
