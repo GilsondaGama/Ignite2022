28cfa7cd7870d10befefb0e7a3e3cc00ed5e0a81
Gilson da Gama <gilsondagama@hotmail.com>
2022-06-24 01:30:32 -0300

    Realizando likes nos comentários

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,17 +1,20 @@
 import { Trash, ThumbsUp } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content);
-    // setComments(comments.filter((_, i) => i !== index))
+  }
+
+  function handleLikeComment() {
+    setLikeCount( likeCount + 1)
   }
   
-
-
-
   return (
     <div className={styles.comment}>
       <Avatar 
@@ -39,9 +42,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
