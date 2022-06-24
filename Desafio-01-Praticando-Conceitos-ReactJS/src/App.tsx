8a132595ccb9b189149b849330edae57046cab74
Gilson da Gama <gilsondagama@hotmail.com>
2022-06-24 15:32:14 -0300

    Iniciando o projeto: Desafio 01 - Praticando os conceitos do ReactJS

---
+++ Desafio-01-Praticando-Conceitos-ReactJS/src/App.tsx
@@ -0,0 +1,26 @@
+import styles from './App.module.css'
+import './global.css'
+
+
+const tasks = [
+  {
+
+  },
+]
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <div className={styles.wrapper}>
+        <main>
+          <h1>Hello! World</h1>
+          {/* <TaskList /> */}
+        </main>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
