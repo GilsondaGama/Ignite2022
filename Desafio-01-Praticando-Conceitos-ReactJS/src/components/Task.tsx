a82a43603fb61b0f39d6cd57939f837c1d90e51f
Gilson da Gama <gilsondagama@hotmail.com>
2022-07-01 19:31:37 -0300

    Funcionalidade de Total Geral e total de Tarefas concluídas, finalizado.

--- Desafio-01-Praticando-Conceitos-ReactJS/src/components/Task.tsx
+++ Desafio-01-Praticando-Conceitos-ReactJS/src/components/Task.tsx
@@ -1,4 +1,4 @@
-import { Check, Circle, PlusCircle, Trash } from 'phosphor-react'
+import { PlusCircle, Trash } from 'phosphor-react'
 import { useState, ChangeEvent, FormEvent, InvalidEvent, } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -10,38 +10,39 @@ interface Task {
   isCompleted: boolean;
 }
 
-const initialTasks:Task[] =[
-  {
-    id: uuidv4(),
-    title: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 1',
-    isCompleted: false,
-  },
-  {
-    id: uuidv4(),
-    title: 's nisi ut aliquip ex ea commodo consequat. Duis  2',
-    isCompleted: true,
-  },
-  {
-    id: uuidv4(),
-    title: 'mollit anim id est laborum 3',
-    isCompleted: false,
-  },
-  {
-    id: uuidv4(),
-    title: 'fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 4',
-    isCompleted: true,
-  },
-  {
-    id: uuidv4(),
-    title: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic  5 sometimes known, is dummy text',
-    isCompleted: true,
-  },        
-]
+// const initialTasks:Task[] =[
+//   {
+//     id: uuidv4(),
+//     title: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 1',
+//     isCompleted: false,
+//   },
+//   {
+//     id: uuidv4(),
+//     title: 's nisi ut aliquip ex ea commodo consequat. Duis  2',
+//     isCompleted: true,
+//   },
+//   {
+//     id: uuidv4(),
+//     title: 'mollit anim id est laborum 3',
+//     isCompleted: false,
+//   },
+//   {
+//     id: uuidv4(),
+//     title: 'fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 4',
+//     isCompleted: true,
+//   },
+//   {
+//     id: uuidv4(),
+//     title: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic  5 sometimes known, is dummy text',
+//     isCompleted: true,
+//   },        
+// ]
 
 export function Task() {
-  const [tasks, setTasks] = useState<Task[]>([...initialTasks]);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState(''); 
-
+  const [tasksCompleteds, setTasksCompleteds] = useState(0);
+  
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault()
 
@@ -64,17 +65,20 @@ export function Task() {
     event.target.setCustomValidity('Por favor, digite uma tarefa!')
   }
 
-
   function handleTogleIsCompleted(id: String) {
     const taskToToggle = tasks.find(task => task.id === id)
     if(!taskToToggle) return
     taskToToggle.isCompleted = !taskToToggle.isCompleted
     setTasks([...tasks])
+    setTasksCompleteds(tasks.filter(task => task.isCompleted).length)
   }
 
   function handleDeleteTask(taskToDelete: String) {
     const tasksWithoutDeletedOne = tasks.filter(task => task.id !== taskToDelete)
     setTasks(tasksWithoutDeletedOne)
+
+    setTasksCompleteds(tasksWithoutDeletedOne.filter(task => task.isCompleted).length)
+    // setTasksCompleteds(tasks.filter(task => task.isCompleted).length)    
   }
 
   return (
@@ -98,23 +102,24 @@ export function Task() {
 
       <div className={styles.taskList}>
         <header>
-          <p> Tarefas criadas 5</p>      
-          <p>Concluídas 2 de 5</p>
+          <p>Tarefas criadas {tasks.length}</p>      
+          <p>Concluídas {tasksCompleteds} de {tasks.length}</p>
         </header>
 
         {tasks.map(task => {
           return (
-            <div key={task.id} className={styles.taskItem}>            
-              <input 
-                type="checkbox"
-                className={styles.taskCheckIsComplete}
-                readOnly
-                checked={task.isCompleted}
-                onClick={() => handleTogleIsCompleted(task.id)}
-              />              
+            <div key={task.id} className={styles.taskItem}> 
+              <div className={styles.checkbox}>         
+                <input 
+                  type="checkbox"
+                  // readOnly
+                  checked={task.isCompleted}
+                  onClick={() => handleTogleIsCompleted(task.id)}
+                />              
+              </ div>
 
-              <p className={task.isCompleted ? styles.taskItemPCompleted : styles.taskItemPNotCompleted}>
-                {task.title}
+              <p className={task.isCompleted ? styles.taskTitleCheck : styles.taskTitleNoCheck}>
+                 {task.title}
               </p>
 
               <button
